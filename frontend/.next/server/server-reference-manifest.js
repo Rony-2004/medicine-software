09f85d66b139b09e224f0b6184d8b2d380a91291
MU AHMED <lifeofcoder19@gmail.com>
2025-07-11 14:43:21 +0530

    Remove .next from git tracking

--- frontend/.next/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"pexznmwjSMLf2OMH55QL2FtG7hckxADPJI4hVHbAyXM=\"\n}"
--- frontend/.next/static/chunks/src_app_layout_tsx_c0237562._.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/src_app_globals_b805903d.css",
-  "static/chunks/_0c38fd96._.js"
-],
-    source: "dynamic"
-});
